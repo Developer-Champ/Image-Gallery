cd2832dbe5286e55e2afa5fa7d565235efa4af13
Developer-Champ <azanrizwan0610@gmail.com>
2024-12-13 23:11:38 +0500

    Updated

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
 }
+
+function uploadImage() {
+    const input = document.getElementById('imageUpload');
+    const gallery = document.getElementById('gallery');
+
+    for (let i = 0; i < input.files.length; i++) {
+        const file = input.files[i];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.alt = `Uploaded Image ${i + 1}`;
+            img.className = 'gallery-item';
+            img.onclick = function() { openModal(this); };
+            gallery.appendChild(img);
+        }
+
+        reader.readAsDataURL(file);
+    }
+}
